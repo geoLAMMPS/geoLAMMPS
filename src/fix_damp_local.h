bbeb57387e841cd9b880e3a2b12a908a0762f099
George Marketos <gmarketo@cv-gmarketo1.cv.ic.ac.uk>
2012-10-05 11:40:18 +0100

    FIX DAMP/LOCAL now added (Potyondy&Cundall's local damping model)

---
+++ src/fix_damp_local.h
@@ -0,0 +1,49 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under 
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef FIX_CLASS
+
+FixStyle(damp/local,FixDampLocal)
+
+#else
+
+#ifndef LMP_FIX_DAMP_LOCAL_H
+#define LMP_FIX_DAMP_LOCAL_H
+
+#include "fix.h"
+
+namespace LAMMPS_NS {
+
+class FixDampLocal : public Fix {
+ public:
+  FixDampLocal(class LAMMPS *, int, char **);
+  virtual ~FixDampLocal();
+  int setmask();
+  void init();
+  void setup(int);
+  void post_force(int);
+  void post_force_respa(int, int, int);
+
+  int modify_param(int, char **); // added in so as to be able to reset parameters
+
+ protected:
+  double alpha;
+  int nlevels_respa;
+
+  double signofnum(double);
+};
+
+}
+
+#endif
+#endif
