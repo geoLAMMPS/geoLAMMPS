2a815761a17779ea21f8745d241681c697c10bc6
Kevin Hanley <kh@cvcosulliv01.cv.ic.ac.uk>
2012-12-06 16:01:05 +0000

    Moved new pairstyle to GRANULAR directory and added an entry to the install file.

--- src/GRANULAR/Install.sh
+++ src/GRANULAR/Install.sh
@@ -8,6 +8,7 @@ if (test $1 = 1) then
   cp pair_gran_hertz_history.cpp ..
   cp pair_gran_hooke.cpp ..
   cp pair_gran_hooke_history.cpp ..
+  cp pair_gran_shm_history.cpp ..
 
   cp fix_freeze.h ..
   cp fix_pour.h ..
@@ -15,6 +16,7 @@ if (test $1 = 1) then
   cp pair_gran_hertz_history.h ..
   cp pair_gran_hooke.h ..
   cp pair_gran_hooke_history.h ..
+  cp pair_gran_shm_history.h ..
 
 elif (test $1 = 0) then
 
@@ -24,6 +26,7 @@ elif (test $1 = 0) then
   rm -f ../pair_gran_hertz_history.cpp
   rm -f ../pair_gran_hooke.cpp
   rm -f ../pair_gran_hooke_history.cpp
+  rm -f ../pair_gran_shm_history.cpp
 
   rm -f ../fix_freeze.h
   rm -f ../fix_pour.h
@@ -31,5 +34,6 @@ elif (test $1 = 0) then
   rm -f ../pair_gran_hertz_history.h
   rm -f ../pair_gran_hooke.h
   rm -f ../pair_gran_hooke_history.h
+  rm -f ../pair_gran_shm_history.h
 
 fi
